0edbd917ceae512858b9498000978abe2e427af8
zZzKoTzZz <vk.210@yandex.ru>
2025-01-11 19:53:14 +0300

    Add files via upload

---
+++ 100-js/main.js
@@ -0,0 +1,11 @@
+const numbers = [3, 4, 6, 7, 9]
+        for( let i = 0; i < numbers.length; i++) {
+                if (numbers[i] % 2 === 0) {
+                console.log(numbers[i])
+                }
+        }
+
+const colorsRainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+        for (let i = colorsRainbow.length - 1; i >= 0; i--) {
+                console.log(colorsRainbow[i])
+                }
