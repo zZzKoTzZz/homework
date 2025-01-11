054110c2163de410a4d8f489b8082c07b22f5677
zZzKoTzZz <vk.210@yandex.ru>
2025-01-11 19:19:08 +0300

    Add files via upload

---
+++ 97-js/main.js
@@ -0,0 +1,9 @@
+let myAge = 25 
+
+// if (myAge >= 18) {
+//     console.log('Вы совершеннолетний')
+// } else {
+//     console.log('Вам еще нет 18')
+// }
+
+myAge >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
